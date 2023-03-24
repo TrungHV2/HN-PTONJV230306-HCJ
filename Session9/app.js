fc08392f85ef6de6efe0fc86a43dc89b513a794a
Hoàng Văn Trung <trunghv@bachkhoa-aptech.edu.vn>
2023-03-24 21:33:58 +0700

    Session9

---
+++ Session9/app.js
@@ -0,0 +1,12 @@
+// alert('JavaScript External');
+
+var tenBien;
+var ten_bien;
+var ten, tuoi, diaChi;
+var so = 12345, chuoi = `Chuỗi`, chuoi1 = 'Chuỗi1', chuoi2 = "Chuỗi 2", check = false, $null = null, _undf = undefined;
+
+alert(typeof so);
+alert(typeof chuoi);
+alert(typeof check);
+alert(typeof $null);
+alert(typeof _undf);
